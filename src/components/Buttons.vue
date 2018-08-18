30f6ea0fd5b4b590e643e1c1571551601a870b79
CristoAMH <cristoamh@gmail.com>
2018-08-18 11:27:14 +0100

    Adding Nav and buttons

---
+++ src/components/Buttons.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Here the buttons!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Buttons',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Buttons></Buttons>
+  </div>
+</template>
+
+<script>
+import Buttons from './Buttons'
+export default {
+  name: 'Nav',
+  components : {
+    Buttons
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
