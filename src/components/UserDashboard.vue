6e7f4f35f74c59b568fd64f4cd35d25ba0755853
BryanPlaces <eduardo.places@gmail.com>
2018-08-25 18:44:52 +0100

    coneccion back y front funcional

--- src/components/UserDashboard.vue
+++ src/components/UserDashboard.vue
@@ -1,16 +1,72 @@
 <template>
     <div>
-        <h1>You have spent 20$ on AMAZON S3</h1>
+        <div v-if="publickey != ''">
+            <br><br>
+            <b>Public Key:  </b>{{publickey}}
+            <br>
+            <b>Private Key:  </b>{{privatekey}}
+        </div>
+        <div v-if="messageAlert != ''">
+            {{messageAlert}}
+        </div>
+        <br>
+        <a class="actionButton" v-on:click="awsapiCall()">Costes diarios</a>
+        <br><br>
+        <div v-for="costs in cost" :key="costs.id">
+            Gastado hoy: {{costs.Total.AmortizedCost.Amount}}&nbsp;{{costs.Total.AmortizedCost.Unit}}
+        </div>    
     </div>
-    
 </template>
 
 <script>
+
+import axios from 'axios';
+
     export default {
         name: 'UserDashboard',
         data() {
-            return {};
+            return {
+                user: "",
+                cost:"",
+                publickey: "",
+                privatekey:"",
+                messageAlert:"",
+                activarCostes:""
+            };
         },
+
+        created (){
+            this.getUser();
+        },
+        methods:{
+            getUser() {
+                axios.get(`http://localhost:4000/secure/users/` + this.$route.params.id, {headers:{Authorization:this.$route.params.token}})
+                    .then( res => {
+                        if (res.data.isActive == false) {
+                            this.messageAlert = "Tu correo no está verificado, por favor, verificalo para poder continuar"
+                        }else{
+                            if(res.data.publicAWSKey != null || res.data.publicAWSKey != "" && res.data.privateAWSKey != null || res.data.privateAWSKey != "") {
+                                this.publickey = res.data.publicAWSKey;
+                                this.privatekey = res.data.privateAWSKey;
+                            }
+                        }
+
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            },
+
+            awsapiCall(){
+                axios.get('http://localhost:4000/awsapi/?publicAWSKey='+this.publickey+'&privateAWSKey='+ this.privatekey, {headers:{Authorization:this.$route.params.token}})
+                .then( res =>  {
+                    this.cost=res.data.ResultsByTime
+                })
+                .catch(function (error) {
+                console.log(error);
+                });
+            }
+        }            
     }
 </script>
 
